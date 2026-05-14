61caa4f80f86330bd8167965a44116327edbd87e
Jessica Mulein <jessica@digitaldefiance.org>
2026-05-14 09:51:51 -0700

    5.9.0.17

--- Config/version.mk
+++ Config/version.mk
@@ -27,5 +27,5 @@
 # This must also serve as a shell script, so do not add spaces around the
 # `=' signs.
 
-VERSION=5.9.0.16
-VERSION_DATE='May 12, 2026'
+VERSION=5.9.0.17
+VERSION_DATE='May 14, 2026'
